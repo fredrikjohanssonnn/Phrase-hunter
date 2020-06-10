8d0ee6d425f84dddaaf697e057f50bce59223cf4
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2020-06-10 22:00:23 +0200

    Adding the phrase to the display, not working

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,5 +3,12 @@ class Phrase {
     this.phrase = phrase.toLowerCase();
   }
 
-  addPhraseToDisplay() {}
+  addPhraseToDisplay() {
+    const ul = document.querySelector('#phrase ul');
+    const phrase = this.phrase.split('');
+    const splitLetters = phrase.map(
+      (letter) => `<li class="hide letter ${letter}">${letter}</li>`
+    );
+    ul.appendChild(splitLetters);
+  }
 }
